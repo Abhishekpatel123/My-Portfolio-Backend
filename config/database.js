86ad0615f07a0299c378b25d7afc18e2f3b37a1c
Abhishek <abhipatel8719@gmail.com>
2021-11-20 19:14:26 +0530

    changing db setting

--- config/database.js
+++ config/database.js
@@ -9,11 +9,15 @@ module.exports = ({ env }) => {
         default: {
           connector: "bookshelf",
           settings: {
-            client: "sqlite",
-            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+            client: "postgres",
+            host : config.host,
+            port : config.port,
+            database : config.database,
+            username : config.user,
+            password : config.password
           },
           options: {
-            useNullAsDefault: true,
+            ssl: false,
           },
         },
       },
